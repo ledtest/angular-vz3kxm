8b0662bbee65aaea1aa7e5063a69d3d3f019a6a7
ledtest <3130836035@qq.com>
2021-03-06 15:04:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vz3kxm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vz3kxm)
---
+++ src/app/products.ts
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    name: "Apple iPhone 12 (A2404) 128GB 绿色 支持移动联通电信5G 双卡双待手机",
+    photo:
+      "https://img10.360buyimg.com/n7/jfs/t1/120344/32/14989/43851/5f861a0fEdd59a92d/3c64d4346f9a6cf3.jpg",
+    price: 6799,
+    description: "新品预售"
+  },
+  {
+    name: "Apple iPad mini 5 2019年新款平板电脑 7.9英寸（64G WLAN版/A12芯",
+    photo:
+      "https://img12.360buyimg.com/n7/jfs/t1/16095/31/11571/118007/5c9065a6Ed94fd377/20640a59bf8dc661.jpg",
+    price: 2921,
+    description: "店长推荐"
+  },
+  {
+    name:
+      "【赠卡首月免月租】Apple iPhone 12 (A2404) 128GB 黑色 支持移动联通电信",
+    photo:
+      "https://img13.360buyimg.com/n7/jfs/t1/113960/3/20088/56974/5f861926E5153a0ef/1831cb31ecb63f24.jpg",
+    price: 299,
+    description: "经济实用"
+  },
+  {
+    name: "Apple iPhone 12 mini (A2400) 128GB 白色 手机 支持移动联通电信5G",
+    photo:
+      "https://img14.360buyimg.com/n7/jfs/t1/126123/30/14922/62032/5f86157dE1afd5285/b75ae9c5772be338.jpg",
+    price: 300,
+    description: "经典爆款"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
